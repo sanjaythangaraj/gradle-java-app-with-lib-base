836dcdd48f5142f16a43f425e03afee8147dea7e
sanjaythangaraj <sanjaythangaraj@outlook.in>
2024-08-28 10:01:38 +0530

    initial commit

---
+++ data-model/src/main/java/com/example/MessageModel.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class MessageModel {
+
+    private String message;
+
+    public MessageModel(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
